16227b4fea9225426633ef9a11e4a9d14a3eb160
Кулицкий <@ASU-2.ZSK.BY>
2013-08-14 02:24:34 +1200

    added spring security.

---
+++ src/main/java/sp/controller/AdminController.java
@@ -0,0 +1,21 @@
+package sp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Admin web-interface controller
+ * @author the-ramones
+ */
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+    
+    @RequestMapping(value = {"/","/manager"}, method = RequestMethod.GET) 
+    public String manager(Model model) {
+        return "admin";
+    }
+
+}
