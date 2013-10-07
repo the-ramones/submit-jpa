dbd4d0fa5f5dd7fb1f939d8a59aedba5f5c353af
Кулицкий <@ASU-2.ZSK.BY>
2013-10-08 02:13:50 +1200

    Added admin-manager template

--- src/main/java/sp/controller/AdminController.java
+++ src/main/java/sp/controller/AdminController.java
@@ -13,9 +13,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/admin")
 public class AdminController {
-
+    
     @RequestMapping(value = {"/", "/manager", "/admin"}, method = RequestMethod.GET)
     public String manager(Model model) {
+        
         return "admin";
     }
 }
