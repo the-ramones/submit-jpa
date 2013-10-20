bd47e12693ef83e52d50523eeae15bb333027182
Kulitski Paul <p.kulitski@gmail.com>
2013-10-20 23:55:20 +0300

    Set up basic Security bounds for User Admin Guest Roles. Used default hard-coded user service.
    TODO:
    	1. add db userService
    	2. integrate with custom login/logout forms
    	3. fix flow

--- src/main/java/sp/controller/LoginController.java
+++ src/main/java/sp/controller/LoginController.java
@@ -1,9 +1,14 @@
 package sp.controller;
 
+import javax.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Login controller
@@ -13,17 +18,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
+    protected static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     /*
      * 'login', 'login?login=true', 'login/lohout=true'
      */
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginForm(Model model) {
-        model.addAttribute("view", "login");
-        return "login";
+    public String loginForm(@RequestParam(value = "login", required = false) boolean isLogin,
+            @RequestParam(value = "logout", required = false) boolean isLogout,
+            Model model) {
+        if (isLogin) {
+            model.addAttribute("view", "login");
+            return "login";
+        } else if (isLogout) {
+            model.addAttribute("view", "logout");
+            return "logout";
+        }
+        /*
+         * Default to render 'User Info' page
+         */
+        model.addAttribute("view", "logout");
+        return "logout";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String loginHome(Model model) {
+        // TODO:
         return "home";
+    }
+
+    @RequestMapping(value = "logout", method = RequestMethod.POST)
+    public String logoutPage(HttpSession session, Model model) {
+        try {
+            session.invalidate();
+        } catch (IllegalStateException ex) {
+            logger.warn("User session has been already invalidated", ex);
+        } finally {
+            model.addAttribute("login", "true");
+            return "login";
+        }
     }
 }
